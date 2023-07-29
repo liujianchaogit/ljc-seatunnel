537f14aacebdefc5c65089989942fb670e18fe76
liujianchao <102093174@qq.com>
2023-07-29 11:53:53 +0800

    Initial commit

---
+++ src/main/java/com/ljc/seatunnel/SeatunnelApplication.java
@@ -0,0 +1,13 @@
+package com.ljc.seatunnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatunnelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatunnelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljc/seatunnel/SeatunnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljc.seatunnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatunnelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
